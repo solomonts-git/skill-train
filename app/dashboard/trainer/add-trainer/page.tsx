ecf633b9f340afe65dfbf8ddfa4b6bba58ec7e66
solomon <solomongorevachew@gmail.com>
2023-12-16 07:59:08 -0800

    type corrected++

--- app/dashboard/trainer/add-trainer/page.tsx
+++ app/dashboard/trainer/add-trainer/page.tsx
@@ -162,7 +162,7 @@ const AddTrainer = () => {
             className="p-5 md:mr-4 mb-2 w-full text-slate-950 focus:outline-none rounded-md font-mono font-semibold"
           >
             <option value="">Select trainer title</option>
-            {partners.map((p: Partner) => (
+            {partners.map((p: PartnerT) => (
               <option key={p._id} value={p._id}>
                 {p.organizationname}
               </option>
