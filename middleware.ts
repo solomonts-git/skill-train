84dcc7c581793bed933f45ed240085448987866f
solomon <solomongorevachew@gmail.com>
2023-12-16 16:25:45 -0800

    auth cors added

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,22 @@
 export { default } from "next-auth/middleware";
+import { NextRequest, NextResponse } from "next/server";
 
-export const config = { matcher: ["/dashboard/:path*"] };
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export function middleware(request: NextRequest) {
+  if (request.method === "OPTIONS") {
+    return NextResponse.json({}, { headers: corsHeaders });
+  }
+  const response = NextResponse.next();
+  Object.entries(corsHeaders).forEach(([key, value]) => {
+    response.headers.append(key, value);
+  });
+
+  return response;
+}
+
+export const config = { matcher: ["/dashboard/:path*", "/api/:path*"] };
